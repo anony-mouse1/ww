7e3648b703a7f3764ebcef01e78eca268e2bf69a
anony-mouse1 <mindblowingmaths@gmail.com>
2023-09-27 00:09:42 -0700

    wtf am i doing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import tempfile
 
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 #demo video 
 # DEMO_VIDEO = 'ri1.mp4'
@@ -31,99 +32,18 @@ def calculate_angle(a, b, c):
     return angle
 
 
-def main():
+class VideoTransformer(VideoTransformerBase):
+    def recv(self, frame): # i think self, frame --> ret, image
+        image = frame.to_ndarray(format="rgb24")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mp_drawing = mp.solutions.drawing_utils
+        mp_pose = mp.solutions.pose
 
-    #title 
-    st.title('Workout Wiz')
-
-    #sidebar title
-    st.sidebar.title('Choose your exercise')
-
-    # st.sidebar.subheader('Parameters')
-    #creating a button for webcam
-    use_webcam = st.sidebar.button('Use Webcam')
-    #creating a slider for detection confidence 
-    # detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
-    
-    #model selection 
-    model_selection = st.sidebar.selectbox('Model Selection',options=['Bicep Curls','Squats','Plank'])
-
-    st.markdown(' ## Output')
-    stframe = st.empty()
-    
-    #file uploader
-    # video_file_buffer = st.sidebar.file_uploader("Upload a video", type=[ "mp4", "mov",'avi','asf', 'm4v' ])
-
-    
-    #temporary file name 
-    # tfflie = tempfile.NamedTemporaryFile(delete=False)
-
-    # if not video_file_buffer:
-
-    #     if use_webcam:
-    #         vid = cv2.VideoCapture(0)
-    #     else:
-    #         vid = cv2.VideoCapture(0)
-            # vid = cv2.VideoCapture(DEMO_VIDEO)
-            # tfflie.name = DEMO_VIDEO
-    
-    # else:
-    #     tfflie.write(video_file_buffer.read())
-    #     vid = cv2.VideoCapture(tfflie.name)
-
-    #values 
-    # width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-    # height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    # fps = int(vid.get(cv2.CAP_PROP_FPS))
-    #codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
-    # codec = cv2.VideoWriter_fourcc('V','P','0','9')
-    # out = cv2.VideoWriter('output1.webm', codec, fps, (width, height))
-
-
-    # st.sidebar.text('Input Video')
-    # st.sidebar.video(tfflie.name)
-    
-
-    # original = Image.open(image)
-
-
-
-    # drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-
-    mp_drawing = mp.solutions.drawing_utils
-    mp_pose = mp.solutions.pose
-
-    col1, col2 = st.columns(2)
-
-    vid = cv2.VideoCapture(0)
-    instructor = cv2.VideoCapture('squat.mp4')
-
-    if instructor.isOpened():
-        ret2, image2 = instructor.read()
-        
-        if ret2:
-            with col2:
-                st.video('squat.mp4')
-    
-    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose1, \
-        mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose2:
-        
-        while vid.isOpened() and instructor.isOpened():
-            
-            ret, image = vid.read()
-            resized_frame = cv2.resize(image, (600,800))
-                   
-            # ret2, image2 = instructor.read()
-
-  
-            if not ret:
-                break
-            
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            # image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-
+   
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose1, \
+            mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose2:
             results = pose1.process(image) #previously image_rgb
-            results2 = pose2.process(image2)
+    #         results2 = pose2.process(image2)
 
             if results.pose_landmarks:
                 landmarks1 = results.pose_landmarks.landmark
@@ -145,15 +65,104 @@ def main():
                 else:
                     cv2.putText(image, 'INCORRECT FORM', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
             
-            with col1:
+        return (image)
+
+
+
+def main():
+
+    #title 
+    st.title('Workout Wiz')
+
+    #sidebar title
+    st.sidebar.title('Choose your exercise')
+
+    # st.sidebar.subheader('Parameters')
+    #creating a button for webcam
+    use_webcam = st.sidebar.button('Use Webcam')
+    #creating a slider for detection confidence 
+    # detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
+    
+    #model selection 
+    model_selection = st.sidebar.selectbox('Model Selection',options=['Bicep Curls','Squats','Plank'])
+
+    st.markdown(' ## Output')
+    stframe = st.empty()
+
+    webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+
+
+    # st.sidebar.text('Input Video')
+    # st.sidebar.video(tfflie.name)
+    
+
+    # original = Image.open(image)
+
+
+
+    # drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+
+# HEEE HEE
+    # mp_drawing = mp.solutions.drawing_utils
+    # mp_pose = mp.solutions.pose
+
+    # col1, col2 = st.columns(2)
+
+    # vid = cv2.VideoCapture(0)
+    # instructor = cv2.VideoCapture('squat.mp4')
+
+    # if instructor.isOpened():
+    #     ret2, image2 = instructor.read()
+        
+    #     if ret2:
+    #         with col2:
+    #             st.video('squat.mp4')
+    
+    # with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose1, \
+    #     mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose2:
+        
+    #     while vid.isOpened() and instructor.isOpened():
+            
+    #         ret, image = vid.read()
+    #         resized_frame = cv2.resize(image, (600,800))
+                   
+    #         # ret2, image2 = instructor.read()
+
+  
+    #         if not ret:
+    #             break
+            
+    #         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #         # image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+
+    #         results = pose1.process(image) #previously image_rgb
+    #         results2 = pose2.process(image2)
+
+    #         if results.pose_landmarks:
+    #             landmarks1 = results.pose_landmarks.landmark
+    #             shoulder1 = [int(landmarks1[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x * image.shape[1]),
+    #                      int(landmarks1[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y * image.shape[0])]
+    #             elbow1 = [int(landmarks1[mp_pose.PoseLandmark.LEFT_ELBOW.value].x * image.shape[1]),
+    #                     int(landmarks1[mp_pose.PoseLandmark.LEFT_ELBOW.value].y * image.shape[0])]
+    #             wrist1 = [int(landmarks1[mp_pose.PoseLandmark.LEFT_WRIST.value].x * image.shape[1]),
+    #                   int(landmarks1[mp_pose.PoseLandmark.LEFT_WRIST.value].y * image.shape[0])]
+
+    #             angle1 = calculate_angle(shoulder1, elbow1, wrist1)
+    #             cv2.putText(image, f'Angle: {round(angle1, 2)}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    #             cv2.circle(image, tuple(shoulder1), 15, (255, 255, 255), -1)
+    #             cv2.circle(image, tuple(elbow1), 15, (255, 255, 255), -1)
+    #             cv2.circle(image, tuple(wrist1), 15, (255, 255, 255), -1)
+
+    #             if 32 <= abs(angle1) <= 175:
+    #                 cv2.putText(image, 'YES', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
+    #             else:
+    #                 cv2.putText(image, 'INCORRECT FORM', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+            
+    #         with col1:
                 
-                stframe.image(image,use_column_width=True)
-
-            # if results2.pose_landmarks:
-            #video1 = stframe.image(image,use_column_width=True)
-            #video2 = stframe.image(image2,use_column_width=True)
-
-            #webcam = stframe.image(image)
+    #             stframe.image(image,use_column_width=True)
 
             
         
@@ -162,10 +171,12 @@ def main():
     
         
 
-        vid.release()
-        # instructor.release()
-        # out.release()
-        cv2.destroyAllWindows()
+    #     vid.release()
+    #     # instructor.release()
+    #     # out.release()
+    #     cv2.destroyAllWindows()
+
+# HEE HEE END.
 
 if __name__ == '__main__':
     main()
