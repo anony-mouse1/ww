c5220db8797db048ed4a7b794e2c0729d5f02182
anony-mouse1 <mindblowingmaths@gmail.com>
2023-09-27 00:28:41 -0700

    kdfsmdfksgslsrtkmslk;m

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 from streamlit_webrtc import webrtc_streamer
 
-# webrtc_streamer(key="sample")
+webrtc_streamer(key="sample")
 
-webrtc_streamer(key="snapshot", mode=WebRtcMode.SENDRECV, video_transformer_factory=VideoTransformer,client_settings=WEBRTC_CLIENT_SETTINGS, async_transform=True)
+# webrtc_streamer(key="snapshot", mode=WebRtcMode.SENDRECV, video_transformer_factory=VideoTransformer,client_settings=WEBRTC_CLIENT_SETTINGS, async_transform=True)
