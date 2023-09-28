950b419399bd187314aac8d967cec9be501fc473
anony-mouse1 <mindblowingmaths@gmail.com>
2023-09-27 21:23:55 -0700

    does it work??

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
+## This sample code is from https://www.twilio.com/docs/stun-turn/api
+# Download the helper library from https://www.twilio.com/docs/python/install
+
 import cv2
-import os 
-from twilio.rest import Client
+import numpy as np
+import streamlit as st
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+
+# Then, pass the ICE server information to webrtc_streamer().
+webrtc_streamer(
+  key="webcam",
+  rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+  # ...
+)
 
 
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-from streamlit_webrtc import webrtc_streamer
+# import logging
+# import queue
+# from sample_utils.turn import get_ice_servers
 
-account_sid = os.environ['AC5f866da1acb4b18b1adc169c5d540c90']
-auth_token = os.environ['e9d37da9e2d87f28ef34cd426120a5de']
-client = Client(account_sid, auth_token)
 
-token = client.tokens.create()
 
-webrtc_streamer(key="sample", rtc_configuration={"iceServers":token.ice_servers})
+
+# # Then, pass the ICE server information to webrtc_streamer().
+# webrtc_streamer(
+#   key="webcam",
+#   rtc_configuration={
+#       "iceServers": get_ice_servers()
+#   }
+#   # ...
+# )
 
 # webrtc_streamer(key="snapshot", mode=WebRtcMode.SENDRECV, video_transformer_factory=VideoTransformer,client_settings=WEBRTC_CLIENT_SETTINGS, async_transform=True)
