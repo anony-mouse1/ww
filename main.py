b6d72480daacea91d48118dc6bbd7e072cad5208
anony-mouse1 <mindblowingmaths@gmail.com>
2023-10-01 14:06:27 -0700

    bye

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ import streamlit as st
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
 # Then, pass the ICE server information to webrtc_streamer().
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+token = client.tokens.create()
+
+# Then, pass the ICE server information to webrtc_streamer().
 webrtc_streamer(
-  key="webcam",
-  rtc_configuration={  # Add this config
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
+  # ...
+  rtc_configuration={
+      "iceServers": token.ice_servers
+  }
   # ...
 )
-
 
 
 # import logging
