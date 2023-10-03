974dfe659335d2f2c0f07180bc6141bb43f3a467
anony-mouse1 <mindblowingmaths@gmail.com>
2023-10-02 22:17:03 -0700

    configure stun server

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ import os
 
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="sample")
+webrtc_streamer(key="sample", rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    })
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-
 
 # account_sid = os.environ['TWILIO_ACCOUNT_SID']
 # auth_token = os.environ['TWILIO_AUTH_TOKEN']
